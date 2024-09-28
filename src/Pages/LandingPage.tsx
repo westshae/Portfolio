515930aa7ee917a7e334831e93ab8e42e13d3928
Shae West <shaewest02@gmail.com>
2024-09-28 19:02:42 +1200

    Added image gallery

--- src/Pages/LandingPage.tsx
+++ src/Pages/LandingPage.tsx
@@ -3,23 +3,34 @@ import { useState, useCallback, FC } from 'react';
 import HiringManagerToggle from '../Components/HiringManagerDisplay/HiringManagerToggle';
 import { ShowHiringManagerContent } from '../Components/HiringManagerDisplay/ShowHiringManagerContent';
 import { HideHiringManagerContent } from '../Components/HiringManagerDisplay/HideHiringManagerContent';
+import ImageGallery from '../Components/ImageGallery';
+
+const mockImageList = [
+    'image.png',
+    'image.png',
+    'image.png',
+    'image.png',
+    'image.png',
+    'image.png',
+    'image.png',
+    'image.png',
+]
 
 const LandingPage: FC = () => {
-  const theme = useTheme();
-  
+    const theme = useTheme();
 
-  return (
-    <Container>
-        <HiringManagerToggle/>
-        <ShowHiringManagerContent>
-            <Typography variant='h6'>Content only for hiring manager</Typography>
-        </ShowHiringManagerContent>
-        <HideHiringManagerContent>
-            <Typography variant='h6'>Alt-content for non-hiring managers</Typography>
-        </HideHiringManagerContent>
-        Landing
-    </Container>
-  );
+    return (
+        <Container>
+            <HiringManagerToggle />
+            <ShowHiringManagerContent>
+                <Typography variant='h6'>Content only for hiring manager</Typography>
+            </ShowHiringManagerContent>
+            <HideHiringManagerContent>
+                <Typography variant='h6'>Alt-content for non-hiring managers</Typography>
+            </HideHiringManagerContent>
+            <ImageGallery images={mockImageList} />
+        </Container>
+    );
 };
 
 export default LandingPage;
